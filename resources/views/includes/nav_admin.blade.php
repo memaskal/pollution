25e24b19192cc3639a4210a6f7538f0658b9bccf
memaskal <kalourakos@hotmail.com>
2016-10-01 05:45:02 +0300

    Fixed Typo

--- resources/views/includes/nav_admin.blade.php
+++ resources/views/includes/nav_admin.blade.php
@@ -8,7 +8,7 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="{{ url('admin') }}">Envioromental Monitoring</a>
+            <a class="navbar-brand" href="{{ url('admin') }}">Environmental Monitoring</a>
         </div>
         <!-- Collect the nav links, forms, and other content for toggling -->
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
